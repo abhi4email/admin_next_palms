39cdf9306a4f3ff89645067325ebd425d6c4b8d1
Raj Kumar <raj@delimp.com>
2021-12-23 14:36:40 +0530

    fix

--- src/pages/customer/index.tsx
+++ src/pages/customer/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 
 import Layout from 'Layouts';
@@ -10,16 +9,11 @@ import Layout from 'Layouts';
 export default function Error(): JSX.Element {
   const router = useRouter();
   return (
-    <Layout title="404 Page Not Found">
+    <Layout title="Customer list">
       <Card>
         <CardContent>
-          <h1>404 Page Not Found</h1>
-          <small>The page you were looking for doesn&apos;t exist</small>
-          <CardActions>
-            <Link href="/dashboard">
-              <a>Take me home</a>
-            </Link>
-          </CardActions>
+          <h1>Customer list</h1>
+          <small>customer data grid will be here</small>
         </CardContent>
       </Card>
     </Layout>
